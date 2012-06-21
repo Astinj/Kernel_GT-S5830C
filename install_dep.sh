718d7c65dcd5053785a9f7924206025428e755e2
rafael <rafael@baugis.(none)>
2012-06-20 22:06:43 -0300

    update

--- install_dep.sh
+++ install_dep.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 xterm -title 'Install dependencies' -e '
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get install gcc-arm-linux-gnueabi
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install libncurses5-dev ia32-libs gcc-arm-linux-gnueabi
 sudo apt-get clean
 sudo apt-get autoremove
-
-sudo ln -s /usr/lib/i386-linux-gnu/libX11.so.6 /usr/lib/i386-linux-gnu/libX11.so
 
 echo "Select <No> in next screen!!!"
 echo "hit <Enter> to continue"; read
