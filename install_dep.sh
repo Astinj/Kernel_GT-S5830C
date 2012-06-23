604185043df35def1168f6332f68c623db81a8b8
rafael <rafael@baugis.(none)>
2012-06-23 12:18:46 -0300

    update

--- install_dep.sh
+++ install_dep.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 xterm -title 'Install dependencies' -e '
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get install gnupg flex bison \
-gperf libsdl-dev libesd0-dev libwxgtk2.6-dev build-essential zip curl libncurses5-dev \
-zlib1g-dev valgrind qt3-dev-tools ia32-libs && sudo apt-get clean && sudo apt-get autoremove;
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get install emdebian-tools \
+gnupg flex bison gperf libsdl-dev libesd0-dev libwxgtk2.6-dev build-essential zip \
+curl libncurses5-dev zlib1g-dev valgrind qt3-dev-tools ia32-libs;
+
+sudo apt-get clean && sudo apt-get autoremove
+
+emsetup --arch armel
 
 if [ -f platform_prebuilt/.git/config ]; then
 	git pull https://github.com/android/platform_prebuilt.git
